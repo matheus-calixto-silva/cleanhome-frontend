f3b517ca173c80b48e2203ecaf6067bc7ef44c24
Matheus Calixto <matheuscalixto8@gmail.com>
2024-03-12 01:07:02 -0300

    refactor(types.ts): refactored IClientFormValues

--- src/types.ts
+++ src/types.ts
@@ -8,11 +8,4 @@ export interface IClient {
   coordinateY: number;
 }
 
-export interface IClientFormValues {
-  name: string;
-  username: string;
-  email: string;
-  phone: string;
-  coordinateX: number;
-  coordinateY: number;
-}
+export type IClientFormValues = Omit<IClient, 'id'>;
